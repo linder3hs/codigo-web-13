6819e9a9b471e4325b1641d3d8cbb472f5e47557
linder3hs <linderhassinger00@gmail.com>
2022-02-15 19:56:43 -0500

    semana4-dia2.js

--- semana4/dia2/objetos2.js
+++ semana4/dia2/objetos2.js
@@ -51,8 +51,81 @@ const computadora2 = {
   imprimirDetalle: function () {
     console.log(this.color);
   },
+  mostrarDetalle: function () {
+    // color no pertenece al ambito de la funcion
+    console.log("color", this.color);
+  },
 };
 
 computadora.mostrarDetalle();
 computadora.mostrarDetalleCompleto();
 computadora2.imprimirDetalle();
+computadora2.mostrarDetalle();
+
+//? Objetos con array
+const alumno = {
+  nombre: "Juanito",
+  notas: [10, 5, 8, 9],
+  cursos: ["HTML", "CSS", "JS", "React"],
+};
+
+console.log("Notas", alumno.notas);
+
+//? Crear un array de objejos
+const alumnos = [
+  {
+    nombre: "Luciano",
+    edad: 18,
+    promedio: 15,
+  },
+  {
+    nombre: "El Pepe",
+    edad: 21,
+    promedio: 14,
+    direccin: "Av siempre 123",
+  },
+];
+
+// como puedo imprimir la nota de Luciano
+console.log(`Nota de ${alumnos[0].nombre}`, alumnos[0].promedio);
+
+//* En una web siempre estan presentes los array de objetos
+
+//* creemos una array de laptops
+
+//? Esta estructura tambien es conocida como JSON
+//? JSON: Es un formato de archivos
+const laptops = [
+  {
+    marca: "Lenovo",
+    imagen: "https://logo.com",
+    nombre: "Ide Centre AIO I3",
+    vendedor: "Falabella",
+    precioOferta: 1599,
+    precioNormal: 3599,
+    calificacion: 2,
+    caracteristicas: {
+      procesador: "i3",
+      tarjetaDeVideo: "integrada",
+      discoDuro: "1TB",
+      discoDuroSolido: "no aplica",
+    },
+  },
+  {
+    marca: "Acer",
+    imagen: "https://logo.com",
+    nombre: "AN515 15.6",
+    vendedor: "Falabella",
+    precioOferta: 3399,
+    precioNormal: 4999,
+    calificacion: 5,
+    caracteristicas: {
+      procesador: "i5",
+      tarjetaDeVideo: "GTX 1650",
+      discoDuro: "No tiene",
+      discoDuroSolido: "256gb",
+    },
+  },
+];
+
+console.log(laptops);
