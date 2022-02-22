4ce440bd5e78eda1e1944511bac9f3c7da7a55ec
linder3hs <linderhassinger00@gmail.com>
2022-02-21 20:26:49 -0500

    semana5

--- semana5/dia1/dom.js
+++ semana5/dia1/dom.js
@@ -36,3 +36,20 @@ newDivContent.forEach((element) => {
   // aca solo quiero que imprima algo en consola
   console.log(element);
 });
+
+/**
+ * * Otra forma de acceder a los elementos del DOM es utilizando el ID
+ * TODO: Recuerden que podemos asignarle un id a cualquier elemento de nuestro HTML
+ */
+//* Esta variable solo sera dedicada para mi email
+const inputEmail = document.getElementById("input_email");
+// de esta forma estoy accediendo al value de mi input
+inputEmail.style.backgroundColor = "#f01";
+console.log(inputEmail.value);
+console.log("inputEmail", inputEmail);
+// usando puedo acceder directamente a las propiedades de mi input
+
+const inputPassword = document.getElementById("input_password");
+inputPassword.style.backgroundColor = "#000";
+inputPassword.style.color = "#fff";
+console.log("password", inputPassword.value);
