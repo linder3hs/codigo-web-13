c787fdc7d954b6b0b99394453e83ab000be4a97c
linder3hs <linderhassinger00@gmail.com>
2022-02-22 21:46:07 -0500

    dia2

--- semana5/dia2/form.js
+++ semana5/dia2/form.js
@@ -1,3 +1,11 @@
 function obtenerValoresDelInput(event) {
-  console.log(event);
+  //* event contiene muchos atributos del input
+  //* los atributos mas importante que vamos a ver hoy el value y el name
+  //? value = sera el contenido del input
+  //? name = sera el nombre que le asigne a mi input
+
+  //TODO: event es un objeto y la propiedada que contiene al value y al name se
+  //TODO: llama "target"
+  //! accediendo al value
+  console.log(event.target.value);
 }
