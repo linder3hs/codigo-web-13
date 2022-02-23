6752d60ec589ad8955786e1bd6e79128aecdf2a4
linder3hs <linderhassinger00@gmail.com>
2022-02-22 20:44:10 -0500

    dia2

--- semana5/dia2/eventos.js
+++ semana5/dia2/eventos.js
@@ -8,7 +8,41 @@ function imprimir() {
 // resultado
 
 function calculadorHTML(operacion) {
-  console.log("operacion", operacion);
+  // obtener el valor de los inputs
+  const numero1 = document.querySelector("#numero1").value;
+  const numero2 = document.querySelector("#numero2").value;
+  // validamos que lo datos no esten vacios
+
+  if (numero1 === "" || numero2 === "") {
+    alert("Debe completar los campos");
+    return;
+  }
+
+  // una vez que validate los campos debo hallar la operacion
+  // necesito una variable que se encargue de guardar el resultado
+  let resultado = 0;
+  if (operacion === "+") {
+    resultado = sumar(numero1, numero2);
+  }
+
+  if (operacion === "-") {
+    resultado = restar(numero1, numero2);
+  }
+
+  if (operacion === "*") {
+    resultado = multiplicar(numero1, numero2);
+  }
+
+  if (operacion === "/") {
+    resultado = dividir(numero1, numero2);
+  }
+
+  // para acabar debo mostrar el resultado en mi html
+  const p = document.createElement("p");
+  const value = document.createTextNode(resultado);
+  p.appendChild(value);
+  const contenedor = document.querySelector("#resultado");
+  contenedor.appendChild(p);
 }
 
 // function sumar() {
