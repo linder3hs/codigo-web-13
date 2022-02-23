2963174d1a846eb223771bbb6bae9ba006fa4d89
linder3hs <linderhassinger00@gmail.com>
2022-02-22 20:28:50 -0500

    dia2

--- semana5/dia2/eventos.js
+++ semana5/dia2/eventos.js
@@ -3,28 +3,36 @@ function imprimir() {
   console.log("Hola mundo desde una function");
 }
 
-function sumar() {
-  const numero1 = document.querySelector("#numero1").value;
-  const numero2 = document.querySelector("#numero2").value;
+// La idea es tener una funcion que reciba un parametro el cual me indique si es + - * /
+// Em base a ese parametro llama a la funcion que necesite del archivo operaciones y retorno
+// resultado
 
-  //contenedor para imprimir mi resultado
-  const contenedor = document.querySelector("#resultado");
-
-  // validar si es que los inputs estan vacios
-  if (numero1 === "" || numero2 === "") {
-    alert("Debe completar los numeros para continuar");
-    //  cuando los datos esten vacio yo debo detener la ejecucion de mi funcion
-    return;
-  }
-  // estamos haciendo la operacion para poder
-  // calcumar la suma de los inputs
-  const suma = +numero1 + +numero2;
-
-  // como creo una etique p en js?
-  const p = document.createElement("p"); //* <p></p>
-  // como puede asignar un valor a la etiqueta creada?
-  const pValor = document.createTextNode(suma);
-  // como agrego el pValor al p?
-  p.appendChild(pValor);
-  contenedor.appendChild(p);
+function calculadorHTML(operacion) {
+  console.log("operacion", operacion);
 }
+
+// function sumar() {
+//   const numero1 = document.querySelector("#numero1").value;
+//   const numero2 = document.querySelector("#numero2").value;
+
+//   //contenedor para imprimir mi resultado
+//   const contenedor = document.querySelector("#resultado");
+
+//   // validar si es que los inputs estan vacios
+//   if (numero1 === "" || numero2 === "") {
+//     alert("Debe completar los numeros para continuar");
+//     //  cuando los datos esten vacio yo debo detener la ejecucion de mi funcion
+//     return;
+//   }
+//   // estamos haciendo la operacion para poder
+//   // calcumar la suma de los inputs
+//   const suma = +numero1 + +numero2;
+
+//   // como creo una etique p en js?
+//   const p = document.createElement("p"); //* <p></p>
+//   // como puede asignar un valor a la etiqueta creada?
+//   const pValor = document.createTextNode(suma);
+//   // como agrego el pValor al p?
+//   p.appendChild(pValor);
+//   contenedor.appendChild(p);
+// }
