0cde2e171d15e458e5da90456ec2ca176db648c7
linder3hs <linderhassinger00@gmail.com>
2022-02-23 22:51:25 -0500

    validacion de campos

--- semana5/dia3/events.js
+++ semana5/dia3/events.js
@@ -2,7 +2,15 @@
 //* La forma de JS moderno me permite agregar los eventos de una forma
 //* mas legible
 const btnPrueba = document.querySelector("#btn-prueba");
-const btnPregunta = document.querySelector("#btn-pregunta");
+
+function showAlert(icon, title, text) {
+  Swal.fire({
+    icon,
+    title,
+    text,
+    footer: "<a href='https://google.com'>Link de footer</a>",
+  });
+}
 
 btnPrueba.onclick = function (event) {
   console.log("click desde el boton");
@@ -10,8 +18,31 @@ btnPrueba.onclick = function (event) {
   Swal.fire("Hola mundo");
 };
 
+const btnPregunta = document.querySelector("#btn-pregunta");
+
 btnPregunta.onclick = function () {
   Swal.fire("Alguna pregunta", "Aca el texto de la pregunta", "question");
+};
+
+const btnError = document.querySelector("#btn-error");
+
+btnError.onclick = function () {
+  showAlert("error", "Hubo un error", "Email or Password erroneos");
+};
+
+const btnWarning = document.querySelector("#btn-warning");
+btnWarning.onclick = function () {
+  showAlert("warning", "Hay una alerta de peligro", "El peligro se acerca");
+};
+
+const btnSuccess = document.querySelector("#btn-success");
+btnSuccess.onclick = function () {
+  showAlert("success", "Todo ok", "Todo salio bien");
+};
+
+const btnInfo = document.querySelector("#btn-info");
+btnInfo.onclick = function () {
+  showAlert("info", "Informacion", "Mensaje informativo de mi alerta");
 };
 
 const paises = document.querySelector("#select-opciones");
