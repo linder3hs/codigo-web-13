a779d8a18deb83a05a81e7718ec090d338e35df6
linder3hs <linderhassinger00@gmail.com>
2022-02-23 21:56:28 -0500

    validacion de campos

--- semana5/dia3/events.js
+++ semana5/dia3/events.js
@@ -6,11 +6,34 @@ const btnPrueba = document.querySelector("#btn-prueba");
 btnPrueba.onclick = function (event) {
   console.log("click desde el boton");
   console.log(event);
+  Swal.fire("Hola mundo");
 };
 
 const paises = document.querySelector("#select-opciones");
 
 // onchange: Sirve para detectar si hay un cambio en mi elemento
 paises.onchange = function (event) {
-  console.log("probamos el evento onchange");
+  // de evento quiero obtener el value y options
+  const { value, options } = event.target;
+
+  console.log("valor de event", value);
+  const index = options.selectedIndex;
+  console.log("index", index);
+  console.log("texto", options[index].text);
 };
+
+// const usuario = {
+//   nombre: "Pepe",
+//   apellido: "Zapata",
+//   edad: 30,
+//   direccion: {
+//     calle: "Calle falsa",
+//     numero: "123",
+//     ciudad: "Madrid",
+//   },
+// };
+
+// // Estoy lo puedo descomponer en una variable
+// // Estos es conocido como destructuracion de objetos
+// const { nombre, apellido, edad, direccion } = usuario;
+// console.log(nombre); // Pepe
