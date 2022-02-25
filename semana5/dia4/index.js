78d185c1c3b1eb28d96eef5a5f8551fa0757a3e5
linder3hs <linderhassinger00@gmail.com>
2022-02-24 22:34:32 -0500

    add sweet alert

--- semana5/dia4/index.js
+++ semana5/dia4/index.js
@@ -40,7 +40,7 @@ const barChart = new Chart(contenedorBarChart, {
 const containerDoughnutChart = document
   .querySelector("#doughnut-chart")
   .getContext("2d");
-
+// Estos es para los datos
 const dataForChart = {
   labels: ["Computadoras", "Celulares", "Refrigeradoras", "Parlantes"],
   datasets: [
@@ -51,10 +51,45 @@ const dataForChart = {
     },
   ],
 };
-
+// Esto es para crear el tipo y dar la configuracion de datos
 const config = {
   type: "doughnut",
   data: dataForChart,
 };
-
+// Esto es para la creacion
 const donutChart = new Chart(containerDoughnutChart, config);
+
+const containerLineChart = document
+  .querySelector("#lineal-chart")
+  .getContext("2d");
+
+const configLineal = {
+  type: "line",
+  data: dataForChart,
+};
+
+const lineChart = new Chart(containerLineChart, configLineal);
+
+const containerBubbleChart = document
+  .querySelector("#bubble-chart")
+  .getContext("2d");
+// he tomado a dataForCharty estoy accediendo a la propiedad data y cambio su valor
+dataForChart.datasets[0].data = [
+  {
+    x: 20,
+    y: 30,
+    r: 15,
+  },
+  {
+    x: 40,
+    y: 10,
+    r: 10,
+  },
+];
+
+const configBubble = {
+  type: "bubble",
+  data: dataForChart,
+};
+
+const bubbleChart = new Chart(containerBubbleChart, configBubble);
