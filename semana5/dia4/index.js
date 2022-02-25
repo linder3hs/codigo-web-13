16730bc5638a67dbb519c0c4eb5038c45c7e2e27
linder3hs <linderhassinger00@gmail.com>
2022-02-24 22:10:51 -0500

    add sweet alert

--- semana5/dia4/index.js
+++ semana5/dia4/index.js
@@ -23,8 +23,38 @@ const barChart = new Chart(contenedorBarChart, {
         data: [20, 40, 10, 100, 50, 75, 200],
         borderWidth: 3,
         borderColor: "#f01",
-        backgroundColor: "#ab1",
+        backgroundColor: [
+          "rgba(0, 255, 265, 0.7)",
+          "#f01",
+          "#ba1",
+          "#dac",
+          "#eda",
+          "#a34",
+          "#234",
+        ],
       },
     ],
   },
 });
+
+const containerDoughnutChart = document
+  .querySelector("#doughnut-chart")
+  .getContext("2d");
+
+const dataForChart = {
+  labels: ["Computadoras", "Celulares", "Refrigeradoras", "Parlantes"],
+  datasets: [
+    {
+      label: "PRODUCTOS ELECTRONICOS",
+      data: [100, 230, 520, 267],
+      backgroundColor: ["#f01", "green", "yellow", "orange"],
+    },
+  ],
+};
+
+const config = {
+  type: "doughnut",
+  data: dataForChart,
+};
+
+const donutChart = new Chart(containerDoughnutChart, config);
