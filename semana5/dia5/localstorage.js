be720f076e87150f019a463780c2f55ffba71077
linder3hs <linderhassinger00@gmail.com>
2022-02-25 20:28:20 -0500

    vierne localstorage

--- semana5/dia5/localstorage.js
+++ semana5/dia5/localstorage.js
@@ -12,3 +12,21 @@ btnLogin.onclick = function (event) {
     localStorage.setItem(input.name, input.value);
   });
 };
+
+//* Vamos a obtener los datos de localStorage
+//* la forma que en que obtenemos los datos es
+//* localStorage.getItem(key)
+const email = localStorage.getItem("email");
+const password = localStorage.getItem("password");
+
+const form = document.querySelector("#section-login");
+const titleEmail = document.querySelector("#email");
+const titlePassword = document.querySelector("#password");
+
+if (email !== "" && password !== "") {
+  // El usario ya esta logueado
+  // Si el usario esta logueado voy a ocultar el form
+  form.style.display = "none";
+  titleEmail.innerHTML = email;
+  titlePassword.innerHTML = password;
+}
