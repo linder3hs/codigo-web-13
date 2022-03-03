f5458beea484e8ac491310dfc9bb423c62f67072
linder3hs <linderhassinger00@gmail.com>
2022-03-02 22:59:11 -0500

    reto8

--- semana6/dia3/pokedex.js
+++ semana6/dia3/pokedex.js
@@ -27,8 +27,10 @@ const setPokemonsInView = (results) => {
 
     // cuando index = 0 + 1
     // index = 1 + 1
+    // !aca usamos await porque getColorPokemon es una funcion async por
+    // !por ende no se cuanto demore en reponder
     const bgColor = await getColorPokemon(index + 1);
-    console.log("bgColor", bgColor);
+
     const html = `
       <div class="col-md-3 mt-3">
         <div class='card' style='background-color: ${bgColor};'>
@@ -39,7 +41,7 @@ const setPokemonsInView = (results) => {
             src='${imgUrl}${index + 1}.svg'
           >
           <div class='card-body text-center'>
-            <h6 class='text-title text-secondary'>N° ${index + 1}</h6>
+            <h6 class='text-title text-white'>N° ${index + 1}</h6>
             <h4 class='text-title'>${result.name}</h4>
           </div>
           <button class='btn btn-primary' onclick='obtenerDetallePokemon("${
@@ -64,6 +66,16 @@ const obtenerDetallePokemon = async (url) => {
 // llamamos a la funcion
 obtenerPokemones();
 
+const color = {
+  red: "rgba(255, 48, 50, 0.7)",
+  green: "rgba(83, 180, 50, 0.7)",
+  blue: "rgba(113, 104, 226, 0.7)",
+  brown: "rgba(113, 104, 226, 0.7)",
+  purple: "rgba(113, 104, 226, 0.7)",
+  pink: "rgba(255, 48, 50, 0.7)",
+  yellow: "rgba(229, 216, 0, 0.6)",
+};
+
 const getColorPokemon = async (id) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${id}`
@@ -71,11 +83,14 @@ const getColorPokemon = async (id) => {
 
   const data = await response.json();
 
-  const color = {
-    red: "rgba(255, 48, 50, 0.7)",
-    green: "rgba(83, 180, 50, 0.7)",
-    blue: "rgba(113, 104, 226, 0.7)",
-  };
+  // Problema el color que nos devuelve el API esta feo
+  // para poder utilizar un color como alternativa
+  // creamos un diccionario de color donde el key sera el color
+  // que devuevla nuestro API
 
+  // si data.color.name = red
+  // buscar dentro del objeto color por el key
+  // como el key es red esto retorna rgba(255, 48, 50, 0.7)
+  console.log(data.color.name);
   return color[data.color.name];
 };
