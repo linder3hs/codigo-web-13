b484b11381cfea159a30d9966d712de03788d149
linder3hs <linderhassinger00@gmail.com>
2022-03-05 11:03:14 -0500

    rick and morty

--- semana6/dia5/reto10/detalle.js
+++ semana6/dia5/reto10/detalle.js
@@ -5,6 +5,15 @@ const imgDetail = document.querySelector("#img-detail");
 const statusCharacter = document.querySelector("#status");
 const nameCharacter = document.querySelector("#name");
 
+const locationCharacter = document.querySelector("#location");
+const origin = document.querySelector("#origin");
+const episodes = document.querySelector("#episodes");
+const species = document.querySelector("#species");
+
 statusCharacter.innerHTML = objetoCharacter.status;
 nameCharacter.innerHTML = objetoCharacter.name;
 imgDetail.src = objetoCharacter.image;
+locationCharacter.innerHTML = objetoCharacter.location.name;
+origin.innerHTML = objetoCharacter.origin.name;
+episodes.innerHTML = objetoCharacter.episodes.length;
+species.innerHTML = objetoCharacter.species;
