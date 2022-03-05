77e178c7b132d055b8d181c05b0da0a7670b34cd
linder3hs <linderhassinger00@gmail.com>
2022-03-04 22:07:59 -0500

    rick and morty

---
+++ semana6/dia5/reto10/index.js
@@ -0,0 +1,46 @@
+const containerElements = document.querySelector("#container-elements");
+
+const obtenerData = async () => {
+  const reponse = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await reponse.json();
+  renderData(data.results);
+};
+
+const renderData = (data) => {
+  data.map((character) => {
+    const html = `
+    <div class="col-md-3 mb-5">
+          <div class="card">
+            <div
+              class="card-body card-custom-body"
+              style="
+                background: linear-gradient(
+                    0deg,
+                    rgba(0, 0, 0, 0.3),
+                    rgba(0, 0, 0, 0.3)
+                  ),
+                  url(${character.image});
+              "
+            >
+              <span class="badge bg-success">Alive</span>
+              <h4 class="text-white mt-2">${character.name}</h4>
+              <div class="d-flex justify-content-between w-100 text-white">
+                <div>
+                  <span
+                    ><i class="fa-solid fa-earth-americas"></i>&nbsp;${character.origin.name}</span
+                  >
+                </div>
+                <div>
+                  <span><i class="fa-solid fa-clapperboard"></i>&nbsp;${character.episode.length}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+
+    containerElements.innerHTML += html;
+  });
+};
+
+obtenerData();
