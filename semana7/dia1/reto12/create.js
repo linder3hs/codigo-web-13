25a18724f7feea46e72075c2a41d2296e1cd2538
linder3hs <linderhassinger00@gmail.com>
2022-03-09 19:43:46 -0500

    yt validations

--- semana7/dia1/reto12/create.js
+++ semana7/dia1/reto12/create.js
@@ -14,6 +14,7 @@ selectGender.onchange = function (event) {
 btnCrear.onclick = async function (event) {
   // como evitamos que la pagina se recargue
   event.preventDefault();
+  selectGender.style.border = "1px solid #ccc";
   // al momento de dar click debo obtener el valor de los inputs
   const values = {};
   // Esto sirve para poder agregar un elemento a un objeto
@@ -22,36 +23,62 @@ btnCrear.onclick = async function (event) {
   //  "key": "value"
   // }
 
+  // ahora debemos validar que los inputs y el select esten llenos
+  // vamos a crear una varaible que sea true  y si en el forEach hay algun campo vacio esta pasa a ser false
+  let validacionInputs = true;
   inputs.forEach((input) => {
+    input.style.border = "1px solid #ccc";
+
+    if (input.value === "") {
+      validacionInputs = false;
+      input.style.border = "1px solid #f01";
+    }
+
     values[input.name] = input.value;
   });
 
+  if (valueGender === "") {
+    selectGender.style.border = "1px solid #f01";
+    validacionInputs = false;
+  }
+
+  if (!validacionInputs) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Debe completar los campos",
+    });
+    return;
+  }
+
+  console.log("validacion", validacionInputs);
+
   // para poder crear nuestro elemento en el API necesitamos armar un objeto
   // este objeto sera que el que enviaremos al API para que pueda crear una nueva pelicuala
-  const movie = {
-    name: values.movie_name,
-    director: values.movie_director,
-    wallpaper: values.movie_photo,
-    gender: valueGender,
-    video_link: values.movie_video,
-  };
+  // const movie = {
+  //   name: values.movie_name,
+  //   director: values.movie_director,
+  //   wallpaper: values.movie_photo,
+  //   gender: valueGender,
+  //   video_link: values.movie_video,
+  // };
 
-  // Ahora vamos a ejecutar una peticion con el motodo POST porque vamos a crear algo
-  // Sabemos que por default el fetch es de metodo GET
-  // ahora para cambiarlos a un POST necesitamos pasarle un objeto indicando su METODO
-  // al decirle que method ahora es POST automaticamente podremos pasarle un body
-  // body: Es el objeto que vamos a enviar al servidor para que pueda ser creado
-  // body solo recibe strings
-  const response = await fetch(URL, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(movie),
-  });
+  // // Ahora vamos a ejecutar una peticion con el motodo POST porque vamos a crear algo
+  // // Sabemos que por default el fetch es de metodo GET
+  // // ahora para cambiarlos a un POST necesitamos pasarle un objeto indicando su METODO
+  // // al decirle que method ahora es POST automaticamente podremos pasarle un body
+  // // body: Es el objeto que vamos a enviar al servidor para que pueda ser creado
+  // // body solo recibe strings
+  // const response = await fetch(URL, {
+  //   method: "POST",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(movie),
+  // });
 
-  const data = await response.json();
+  // const data = await response.json();
 
-  console.log("data", data);
+  // console.log("data", data);
 };
