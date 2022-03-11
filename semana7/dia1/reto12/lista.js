98007c691ad799975f343b6369966404827dbed5
linder3hs <linderhassinger00@gmail.com>
2022-03-10 20:39:19 -0500

    udpate service

--- semana7/dia1/reto12/lista.js
+++ semana7/dia1/reto12/lista.js
@@ -2,6 +2,9 @@
 
 const tableContainer = document.querySelector("#table-container");
 
+// boton para actualizar
+const btnActualizar = document.querySelector("#btn-actualizar");
+
 // inputs del modal
 movieName = document.querySelector(".movie-name");
 movieDirector = document.querySelector(".movie-director");
@@ -9,14 +12,40 @@ movieGender = document.querySelector(".movie-gender");
 moviePhoto = document.querySelector(".movie-photo");
 movieVideo = document.querySelector(".movie-video");
 
+let idMovie = "";
+
 //comentario de prueba
 const getDataFromMovie = async (movieId) => {
+  // cada vez que hagamos click en el boton editar vamos
+  // a cambiar el valor de la variable idMovie por el movieId
+  idMovie = movieId;
   const movie = await getMovieDetail(movieId);
   movieName.value = movie.name;
   movieDirector.value = movie.director;
   movieGender.value = movie.gender;
   moviePhoto.value = movie.wallpaper;
   movieVideo.value = movie.video_link;
+};
+
+btnActualizar.onclick = async function () {
+  // primero debemos contruir nuestro objeto
+  const movie = {
+    name: movieName.value,
+    director: movieDirector.value,
+    wallpaper: moviePhoto.value,
+    gender: movieGender.value,
+    video_link: movieVideo.value,
+  };
+  await updateMovie(idMovie, movie);
+
+  Swal.fire({
+    icon: "success",
+    title: "Todo bien",
+    text: "Se actualizo la pelicula",
+  });
+
+  // vamos a actalizat la pagina despues de hacer el update
+  window.location.reload();
 };
 
 const renderMovies = async () => {
