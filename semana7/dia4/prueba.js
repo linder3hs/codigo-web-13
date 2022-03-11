99b1dfc9c216282e1b688b15b415232a86ccb912
linder3hs <linderhassinger00@gmail.com>
2022-03-10 22:19:58 -0500

    udpate service

---
+++ semana7/dia4/prueba.js
@@ -0,0 +1,8 @@
+// Este archivo sera la prueba de como poder ejecutar un archivo de JS de la consola/terminal
+
+const num1 = 10;
+const num2 = 20;
+
+const suma = num1 + num2;
+
+console.log("La suma es", suma);
