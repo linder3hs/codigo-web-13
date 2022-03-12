1d9eb4ce465a25eb2949094a0332396d5a24554b
linder3hs <linderhassinger00@gmail.com>
2022-03-11 22:42:25 -0500

    init

--- semana7/dia4/index.js
+++ semana7/dia4/index.js
@@ -6,6 +6,9 @@ import cors from "cors";
 import { arrayMovies } from "./movies.js";
 
 const app = express();
+// Esto sirve para poder leer los JSON que envie el cliente
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 // para poder crear nuestras rutas
 // para este ejemplo vamos a crear un archivo llamando movies.js
@@ -13,6 +16,32 @@ app.use(cors());
 // vamos a crear una ruta movies para poder listar nuestras peliculas
 app.get("/movies", (req, res) => {
   res.json(arrayMovies);
+});
+
+// ahora haremos la funcion que busca un pelicula
+// recuerden que debemos recibir un id y en base a ese id buscar la pelicula
+app.get("/movies/:id", (req, res) => {
+  // ahora la forma de obtener el valor de la url es con req
+  // params permite acceder a los valores de la url
+  const id = req.params.id;
+  const movie = arrayMovies.find((movie) => movie.id === +id);
+  res.json(movie);
+});
+
+app.post("/movies", (req, res) => {
+  // en donde se guarda la informacion que envia el cliente?
+  // se guarda en req
+  // la forma en la cual accedemos a la data que envie el cliente usando body
+  // req.body
+  const data = req.body;
+  const id = arrayMovies.length + 1;
+  // como agregamos un valor a un objo
+  data.id = id;
+  // como agrego un elemento a un array?
+  arrayMovies.push(data);
+  res.json({
+    message: "Todo bien",
+  });
 });
 
 app.listen(3000, () =>
